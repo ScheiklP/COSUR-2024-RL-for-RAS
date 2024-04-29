09ce608938b9d4a8fb1ea016a53134f1f6886c46
Paul Maria Scheikl <paul.scheikl@kit.edu>
2024-04-29 15:47:03 +0200

    Move coupled PSM joints together.

--- cosur/dvrk_env.py
+++ cosur/dvrk_env.py
@@ -1,3 +1,4 @@
+import time
 import numpy as np
 import pybullet as p
 import pybullet_data
@@ -13,6 +14,17 @@ physicsClient = p.connect(p.GUI)
 
 # Optional: Set additional paths to find the robot URDF files
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
+
+def joint_values_for_pose(pose, frame_id, robot_id):
+    return p.calculateInverseKinematics(
+        bodyUniqueId=robot_id,
+        endEffectorLinkIndex=frame_id,
+        targetPosition=pose[:3],
+        targetOrientation=pose[3:],
+        maxNumIterations=1000,
+    )
+
 
 # Load the plane and robot
 psm_id = p.loadURDF(
@@ -43,6 +55,25 @@ for j in range(p.getNumJoints(psm_id)):
     if joint_type in (p.JOINT_REVOLUTE, p.JOINT_PRISMATIC):
         p.resetJointState(psm_id, j, joint_values[index])
         index = index + 1
+    # Add visual text to the links
+    text_xy = np.random.rand(2) / 10
+    p.addUserDebugText(
+        text=str(j),
+        textPosition=[text_xy[0], text_xy[1], 0],
+        textColorRGB=[0, 0, 1],
+        textSize=1.5,
+        parentObjectUniqueId=psm_id,
+        parentLinkIndex=j,
+    )
+
+    # Add coordinate frames to the links
+    p.addUserDebugLine(
+        lineFromXYZ=[0, 0, 0],
+        lineToXYZ=[0.1, 0, 0],
+        lineColorRGB=[1, 0, 0],
+        parentObjectUniqueId=psm_id,
+        parentLinkIndex=j,
+    )
 
 
 num_ecm_joint = p.getNumJoints(ecm_id)
@@ -57,40 +88,58 @@ for j in range(p.getNumJoints(ecm_id)):
         p.resetJointState(ecm_id, j, joint_values[index])
         index = index + 1
 
+    # Add visual text to the links
+    p.addUserDebugText(
+        text=str(j),
+        textPosition=[0.1, 0, 0],
+        textColorRGB=[0, 1, 0],
+        textSize=1,
+        parentObjectUniqueId=ecm_id,
+        parentLinkIndex=j,
+    )
+
+
+target_position = np.array([0.5, 0.5, 0.5])
+target_orientation = np.array([0.0, 0.0, 0.0, 1.0])
+target_pose = target_position.tolist() + target_orientation.tolist()
+planning_frame = 10
+
 # print(">>> Number of joints in the robot:", num_joints)
 joint_trajectory = np.linspace(0, np.pi / 8, 10000).tolist()
 # joint_trajectory = np.zeros(10000).tolist()
 while True:
-    control_joints = list(range(p.getNumJoints(psm_id)))
-    for j in control_joints:
-        print(">>> Joint:", j)
-        for _ in range(2):
-            for val in joint_trajectory:
-                info = p.getJointInfo(psm_id, j)
-                joint_type = info[2]
-                if joint_type == p.JOINT_REVOLUTE:
-                    p.resetJointState(psm_id, j, val)
-                elif joint_type == p.JOINT_PRISMATIC:
-                    val = val / 10 + 0.2
-                    p.resetJointState(psm_id, j, val)
+    joints = [2, 3, 4, 11, 12]
+    directions = [1, -1, 1, -1, 1]
 
-                p.stepSimulation()
-            joint_trajectory = joint_trajectory[::-1]
+    for val in joint_trajectory:
+        for j, direction in zip(joints, directions):
+            info = p.getJointInfo(psm_id, j)
+            joint_type = info[2]
+            if joint_type == p.JOINT_REVOLUTE:
+                set_val = val * direction
+                p.resetJointState(psm_id, j, set_val)
+            elif joint_type == p.JOINT_PRISMATIC:
+                val = val / 10 + 0.2
+                p.resetJointState(psm_id, j, val)
 
-    control_joints = list(range(p.getNumJoints(ecm_id)))
-    for j in control_joints:
-        for _ in range(2):
-            for val in joint_trajectory:
-                info = p.getJointInfo(ecm_id, j)
-                joint_type = info[2]
-                if joint_type == p.JOINT_REVOLUTE:
-                    p.resetJointState(ecm_id, j, val)
-                elif joint_type == p.JOINT_PRISMATIC:
-                    val = val / 10 + 0.2
-                    p.resetJointState(ecm_id, j, val)
+        p.stepSimulation()
+        time.sleep(1.0)
+    joint_trajectory = joint_trajectory[::-1]
 
-                p.stepSimulation()
-            joint_trajectory = joint_trajectory[::-1]
+    # control_joints = list(range(p.getNumJoints(ecm_id)))
+    # for j in control_joints:
+    #     for _ in range(2):
+    #         for val in joint_trajectory:
+    #             info = p.getJointInfo(ecm_id, j)
+    #             joint_type = info[2]
+    #             if joint_type == p.JOINT_REVOLUTE:
+    #                 p.resetJointState(ecm_id, j, val)
+    #             elif joint_type == p.JOINT_PRISMATIC:
+    #                 val = val / 10 + 0.2
+    #                 p.resetJointState(ecm_id, j, val)
+
+    #             p.stepSimulation()
+    #         joint_trajectory = joint_trajectory[::-1]
 
 
 # Disconnect
